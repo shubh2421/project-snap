9b9950475808d5ab84bfa6a63e7968bab5205956
shubh2421 <72147336+shubh2421@users.noreply.github.com>
2021-01-11 13:27:40 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+dbPassword =  'mongodb+srv://wolfintern:'+ encodeURIComponent('wolfintern') + '@cluster0.opbgt.mongodb.net/wolfintern?retryWrites=true&w=majority';
+module.exports = {
+    mongoURI: dbPassword
+};
